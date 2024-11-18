ca84b9f16b9e42617354b85914bb3d98bf6db1fa
EJSeguro <eduardojosesegurodeoliveira@gmail.com>
2024-11-18 09:06:25 -0300

    Rotas de application e vacancy feitos

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,14 +15,19 @@ Route::middleware(['auth:sanctum', 'role:candidate'])->group(function(){
     Route::delete('/application/{id}',[ApplicationController::class,'removeApplication']);
     Route::get('/application',[ApplicationController::class, 'indexApplication']);
     Route::get('/application/{id}',[ApplicationController::class, 'showApplication']);
+
+    Route::post('/application', [ApplicationController::class, 'store']);
+    Route::delete('/application',[ApplicationController::class,'destroy']);
+    Route::get('/application', [ApplicationController::class, 'indexApplicationsCandidate']);
 });
 
 Route::middleware(['auth:sanctum', 'role:recruiter'])->group(function(){
-    Route::post('/vaccacion',[VacancyController::class,'storeVaccacion']);
-    Route::delete('/vaccacion/{id}',[VacancyController::class,'removeVaccacion']);
-    Route::put('/vaccacion/{id}', [VacancyController::class,'updateVaccacion']);
-    Route::get('/vaccacion',[VacancyController::class, 'indexVaccacion']);
-    Route::get('/vaccacion/{id}',[VacancyController::class, 'showVaccacion']);
+    Route::post('/vaccacion',[VacancyController::class,'store']);
+    Route::delete('/vaccacion/{id}',[VacancyController::class,'destroy']);
+    Route::put('/vaccacion/{id}', [VacancyController::class,'update']);
+
+    Route::get('/applications', [ApplicationController::class,'indexApplicationsAll']);
+    Route::get('/applications/{id}',[ApplicationController::class,'indexApplicationsVacancy']);
 });
 
 Route::middleware(['auth:sanctum'])->group(function(){
@@ -32,6 +37,9 @@ Route::middleware(['auth:sanctum'])->group(function(){
     Route::get('/profile/{id}',[UserController::class, 'showProfile']);
 
     Route::post('/logout', [AuthController::class,'logout']);
+
+    Route::get('/vaccacion',[VacancyController::class, 'index']);
+    Route::get('/vaccacion/{id}',[VacancyController::class, 'show']);
 });
 
 // TODO:Terminar rotas
